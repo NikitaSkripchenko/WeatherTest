f8d2c224c8f28e72a3086194f88be85ed913bcd2
Nikita <frickadelka456@gmail.com>
2020-03-14 19:08:14 +0200

    get api key

---
+++ Utils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  WeatherTest
+//
+//  Created by Trexoz MCL on 14.03.2020.
+//  Copyright © 2020 nktskr. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let weatherAPIkey = "90774b523409eb931353ab27a0c07b80"
+}
