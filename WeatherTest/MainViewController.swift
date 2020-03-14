9ab0ea7dfa13bfbdc426b0787eec08379d99a0c8
Nikita <frickadelka456@gmail.com>
2020-03-14 18:55:59 +0200

    add CoreLocation

---
+++ WeatherTest/MainViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  WeatherTest
+//
+//  Created by Trexoz MCL on 14.03.2020.
+//  Copyright © 2020 nktskr. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class MainViewController: UIViewController {
+
+    let locationManager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "London"
+        setupLocationManager()
+    }
+
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.distanceFilter = 100
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+    }
+
+}
+
+extension MainViewController : CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
+}
+
